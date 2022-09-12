ebb228a3398a52268ed03eed1704553e7d8d8c04
polgaram <burock.ozalp@gmail.com>
2022-09-12 14:28:49 +0300

    Add SocialMedia

---
+++ Core_Proje/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.SocialMedia
+{
+    public class SocialMediaList : ViewComponent
+    {
+        private readonly ISocialMediaService _socialMediaService;
+
+        public SocialMediaList(ISocialMediaService socialMediaService)
+        {
+            _socialMediaService = socialMediaService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _socialMediaService.TGetList();
+            return View(values);
+        }
+    }
+}
